02128bcd8f6817a077a5840458a399e827ca221f
lss555 <chrisheibel7@gmail.com>
2022-05-24 23:44:45 -0600

    Redux persist, middleware safegaurds

--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ import App from './App';
 import './index.scss';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { store } from './store/store'
+import { store, persistor } from './store/store'
+import { PersistGate } from 'redux-persist/integration/react'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+    <PersistGate persistor={persistor} loading={null}>
+      <BrowserRouter>
+          <App />
+      </BrowserRouter>
+    </PersistGate>
   </Provider>
 )
